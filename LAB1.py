ac09c6e7171a88c41442bc7f632b172b8315a4a3
Ddestrei <gryspatryk@gmail.com>
2025-10-07 08:28:53 +0200

    07.10.2025

--- LAB1.py
+++ LAB1.py
@@ -21,49 +21,78 @@ def neural_network(input, weights, bias):
     neurons = weights.shape[0]
     output = np.zeros(neurons)
     for n in range(neurons):
-        output[n] = neuron(input,weights[n],bias)
+        output[n] = neuron(input, weights[n], bias)
     return output
 
 
-
 input = np.array([0.5, 0.75, 0.1])
-weights = np.array([[0.1, 0.1, -0.3],[ 0.1, 0.2, 0.0],[0.0,0.7,0.1],[0.2,0.4,0.0],[-0.3,0.5,0.1]])
+weights = np.array([[0.1, 0.1, -0.3], [0.1, 0.2, 0.0], [0.0, 0.7, 0.1], [0.2, 0.4, 0.0], [-0.3, 0.5, 0.1]])
 print(weights)
 
-neural_network(input,weights,0)
+neural_network(input, weights, 0)
 
 ## ZADANIE 3
 
 print("Zadanie 3")
 
+
 def deep_neural_network(input, weights, bias):
     hidden_layer = neural_network(input, weights, bias)
-    hidden_weights = np.array([[0.7,0.9,-0.4,0.8,0.1],[0.8,0.5,0.3,0.1,0.0],[-0.3,0.9,0.3,0.1,-0.2]])
+    hidden_weights = np.array([[0.7, 0.9, -0.4, 0.8, 0.1], [0.8, 0.5, 0.3, 0.1, 0.0], [-0.3, 0.9, 0.3, 0.1, -0.2]])
     print(hidden_layer)
     print(hidden_weights)
     output_layer = neural_network(hidden_layer, hidden_weights, bias)
     return output_layer
 
+
 input = np.array([0.5, 0.75, 0.1])
-weights = np.array([[0.1, 0.1, -0.3],[ 0.1, 0.2, 0.0],[0.0,0.7,0.1],[0.2,0.4,0.0],[-0.3,0.5,0.1]])
-out = deep_neural_network(input,weights,0)
+weights = np.array([[0.1, 0.1, -0.3], [0.1, 0.2, 0.0], [0.0, 0.7, 0.1], [0.2, 0.4, 0.0], [-0.3, 0.5, 0.1]])
+out = deep_neural_network(input, weights, 0)
 print(out)
+
 
 ## Zadanie 4
 
-class NeuralNetwork:
-
-    def __init__(self, input_size, output_size):
+class Layer:
+    def __init__(self, output_size, input_size):
         self.input_size = input_size
         self.output_size = output_size
-        self.weights = []
-        self.weights.append(np.random.rand(input_size, output_size))
+        self.weights = np.random.rand(output_size, input_size)
 
-    def add_layer(self, n, weight_range):
 
-        pass
+class NeuralNetwork:
+
+    def __init__(self, output_size, input_size):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.layers = []
+        self.layers.append(Layer(output_size, input_size))
+
+    def add_layer(self, n):
+        if len(self.layers) == 1:
+            base_layer = self.layers.pop()
+            self.layers.append(Layer(n, base_layer.input_size))
+            self.layers.append(Layer(base_layer.output_size, n))
+        else:
+            last_layer = self.layers.pop()
+            self.layers.append(Layer(n, last_layer.input_size))
+            self.layers.append(Layer(self.output_size, n))
+
     def predict(self, input):
-        pass
+        output = input
+        for l in self.layers:
+            output = neural_network(output, l.weights, 0)
+        return output
+
     def load_weights(self):
         pass
 
+network = NeuralNetwork(5, 3)
+input = np.array([0.5, 0.75, 0.1])
+print(network.predict(input))
+
+network.add_layer(6)
+print(network.predict(input))
+
+network.add_layer(7)
+print(network.predict(input))
